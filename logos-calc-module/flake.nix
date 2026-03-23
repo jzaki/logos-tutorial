66ef39c7f24cde60ff22cab942c63512b97d0177
Khushboo Mehta <mehtakhushboo@Hotmail.com>
2026-03-23 21:59:59 +0100

    feat: use the standalone nix derivation from module builder instead

--- logos-calc-module/flake.nix
+++ logos-calc-module/flake.nix
@@ -3,11 +3,9 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    logos-nix.url = "github:logos-co/logos-nix";
-    nixpkgs.follows = "logos-nix/nixpkgs";
   };
 
-  outputs = { self, logos-module-builder, nixpkgs, ... }:
+  outputs = { logos-module-builder, ... }:
     logos-module-builder.lib.mkLogosModule {
       src = ./.;
       configFile = ./module.yaml;
